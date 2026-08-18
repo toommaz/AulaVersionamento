90344c7992f168711e97b887aafd09ac01b74b74
TOMAZ <98927575+toommaz@users.noreply.github.com>
2026-08-18 11:02:34 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import { View, Text, Image, StyleSheet, } from 'react-native'
+
+export default function App() {
+  return (
+
+   <View style={css.container}>
+     <Text style={css.textos}>O Melhor Motorola do mundo!</Text>
+     <Text style={css.textos}> Ai liguei a lanterna</Text>
+     <Image style={css.imagem} source={require('./MinhasImagens/Celular.jpg')}/>
+   </View>
+
+  );
+}
+const css = StyleSheet.create({
+  container:{
+    backgroundColor: '#836FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex:1,
+  },
+
+  textos:{
+    fontSize:20,
+    fontWeight:100,
+
+  },
+    imagem:{
+    width: 250,
+    height: 300,
+  },
+})
