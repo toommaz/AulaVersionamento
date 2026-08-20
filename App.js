8482862243090d75409dd64d317fead3c220ab46
TOMAZ <98927575+toommaz@users.noreply.github.com>
2026-08-20 15:23:18 -0300

    Adicionado o atributo TextInput
    Adicionado o atributo TextInput

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import { View, Text, Image, StyleSheet, } from 'react-native'
+import { View, Text, Image, StyleSheet, TextInput } from 'react-native'
 
 export default function App() {
   return (
 
    <View style={css.container}>
-     <Text style={css.textos}>O Melhor Motorola do mundo!</Text>
-     <Text style={css.textos}> Ai liguei a lanterna</Text>
-     <Image style={css.imagem} source={require('')}/>
+      <Text style={css.textos}>O Melhor Motorola do mundo!</Text>
+      <TextInput style={css.entrada} />
+      <Image style={css.imagem} source={require('')}/>
+      <Text style={css.textos}> Ai liguei a lanterna</Text>
    </View>
-
   );
 }
 const css = StyleSheet.create({
@@ -18,11 +18,16 @@ const css = StyleSheet.create({
     justifyContent: 'center',
     flex:1,
   },
-
   textos:{
     fontSize:20,
     fontWeight:100,
-
+  },
+    entrada:{
+    height: 40,
+    width:250,
+    backgroundColor:'#ffffff',
+    margin: 8,
+    borderRadius:10,
   },
     imagem:{
     width: 250,
